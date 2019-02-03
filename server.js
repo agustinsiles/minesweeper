245da7374a326985401d0ad0a76ab899b201a199
Agustin Siles <agustinsiles10@gmail.com>
2019-02-03 17:05:27 -0300

    Heroky deployment test

---
+++ server.js
@@ -0,0 +1,11 @@
+// THIS IS JUST FOR HEROKU
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/minesweeper'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/minesweeper/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
